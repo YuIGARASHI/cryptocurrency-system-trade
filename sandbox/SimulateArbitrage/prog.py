a0152f7bdae208f10bfdfea8b16f1db4544c11b3
igarashi339 <igarashi339@gmail.com>
2020-05-02 14:43:07 +0900

    出力ファイルの構成を見やすく変更

--- sandbox/SimulateArbitrage/prog.py
+++ sandbox/SimulateArbitrage/prog.py
@@ -17,7 +17,8 @@ exchange_name_map = {
     ExchangeType.HUOBI_JP: "HU",
     ExchangeType.GMO: "GMO",
     ExchangeType.BITBANK: "BB",
-    ExchangeType.ZAIF: "ZA"
+    ExchangeType.ZAIF: "ZA",
+    ExchangeType.COINCHECK: "CC"
 }
 crypto_name_map = {
     CryptoType.BTC: "BTC",
@@ -33,8 +34,14 @@ def similate_arbitrage(crypto_type, exchanges, ofs):
         handler_map[exchange] = ExchangeHandler(exchange)
     # 見出し出力
     headline_str = ""
+    aleady_print_flag = {}
     for (ex1, ex2) in permutations(exchanges, 2):
-        headline_str += exchange_name_map[ex1]+"_"+exchange_name_map[ex2]+","
+        if aleady_print_flag.get((ex1, ex2)):
+            continue
+        headline_str += exchange_name_map[ex1] + "_"+exchange_name_map[ex2] + ","
+        headline_str += exchange_name_map[ex2] + "_" + exchange_name_map[ex1] + ","
+        aleady_print_flag[(ex1, ex2)] = True
+        aleady_print_flag[(ex2, ex1)] = True
     print(headline_str)
     ofs.write(headline_str+"\n")
     while True:
@@ -42,6 +49,7 @@ def similate_arbitrage(crypto_type, exchanges, ofs):
         sell_map = {}
         buy_map = {}
         line_str = ""
+        aleady_print_flag = {}
         for exchange in exchanges:
             code, info = handler_map[exchange].fetch_ticker_info(crypto_type)
             sell_map[exchange] = info.best_sell_order
@@ -51,8 +59,13 @@ def similate_arbitrage(crypto_type, exchanges, ofs):
             time.sleep(2)
             continue
         for (ex1, ex2) in permutations(exchanges, 2):
+            if aleady_print_flag.get((ex1, ex2)):
+                continue
             # 10万円当たりの利益に変換
             line_str += str(math.floor((buy_map[ex1] - sell_map[ex2]) * (100000 / sell_map[ex2])))+","
+            line_str += str(math.floor((buy_map[ex2] - sell_map[ex1]) * (100000 / sell_map[ex2]))) + ","
+            aleady_print_flag[(ex1, ex2)] = True
+            aleady_print_flag[(ex2, ex1)] = True
         print(line_str)
         ofs.write(line_str+"\n")
         ofs.flush()
@@ -60,12 +73,13 @@ def similate_arbitrage(crypto_type, exchanges, ofs):
 
 if __name__=='__main__':
     exchanges = [
-        #ExchangeType.ZAIF,
+        ExchangeType.ZAIF,
         ExchangeType.BITBANK,
         ExchangeType.GMO,
         ExchangeType.HUOBI_JP,
-        ExchangeType.LIQUID
+        ExchangeType.LIQUID,
+        ExchangeType.COINCHECK
     ]
-    crypto_type = CryptoType.XRP
+    crypto_type = CryptoType.BTC
     ofs = codecs.open("data/result_" + crypto_name_map[crypto_type] + ".csv", "w")
     similate_arbitrage(crypto_type, exchanges, ofs)
